b3c015de4c4e5b1afebc98b5bcffdaeb9c3247fb
akokush <kokush.arkady@gmail.com>
2025-01-09 13:27:29 -0500

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Arkady Kokush on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetGameTests/SetGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameTests.swift
+//  SetGameTests
+//
+//  Created by Arkady Kokush on 1/9/25.
+//
+
+import Testing
+@testable import SetGame
+
+struct SetGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
